2e40824334be54b561019db3da02feea0e57ba59
0306241291-potulins <0306241291@caothang.edu.vn>
2026-09-21 16:50:51 +0700

    fix: Giai quyet conflict o file server.js

--- backend/data/server.js
+++ backend/data/server.js
@@ -26,9 +26,7 @@ res.json({ success: true, message: "Đã cập nhật Profile" });
 res.status(500).json({ message: "Lỗi ghi file" });
 }
 });
-//const PORT = 5000;
-const PORT = 5001;
-
+const PORT = 5000;
 const notesDir = path.join(__dirname, 'data', 'notes');
 // Khởi tạo thư mục tự động nếu chưa tồn tại
 if (!fs.existsSync(notesDir)) {
